3ac3edea5137802b6a39923a738c0773e7e780bb
donut2008 <anjishnu.adhikary@outlook.com>
2022-04-28 22:51:53 +0530

    2.5.95 (buggy build, broken save function)

--- UWPDrawingCanvas/MainPage.xaml.cs
+++ UWPDrawingCanvas/MainPage.xaml.cs
@@ -78,10 +78,6 @@ namespace UWPDrawingCanvas
             if (e.WindowActivationState == CoreWindowActivationState.Deactivated) AppTitle.Foreground = inactiveForegroundBrush;
             else AppTitle.Foreground = defaultForegroundBrush;
         }
-        private void CustomWidth_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
-        {
-            sender.Text = new String(sender.Text.Where(char.IsDigit).ToArray());
-        }
         private void SizeCancelled(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
             new ToastContentBuilder().AddText("Saving cancelled");
@@ -93,15 +89,118 @@ namespace UWPDrawingCanvas
         }
         private void ComboBoxGone(object sender, RoutedEventArgs e)
         {
-            CustomWidth.IsEnabled = true; CustomHeight.IsEnabled = true;
+            CustomWidth.IsEnabled = true; CustomHeight.IsEnabled = true; CustomSizeSelected.IsEnabled = true;
         }
         private void ComboBoxVisible(object sender, RoutedEventArgs e)
         {
-            CustomWidth.IsEnabled = false; CustomHeight.IsEnabled = false;
+            CustomWidth.IsEnabled = false; CustomHeight.IsEnabled = false; CustomSizeSelected.IsEnabled = false;
         }
-        private void SaveClick(object sender, RoutedEventArgs e)
+        private async void SaveClick(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(FileSavePicker));
+            IReadOnlyList<InkStroke> currentStrokes = DrawingCanvas.InkPresenter.StrokeContainer.GetStrokes();
+            CanvasDevice device = CanvasDevice.GetSharedDevice();
+            CanvasRenderTarget renderTarget = new CanvasRenderTarget(device, SavingWidth, SavingHeight, 96);
+            var SaveDrawing = new FileSavePicker
+            {
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary,
+                SuggestedFileName = "Untitled drawing"
+            };
+            if (currentStrokes.Count > 0)
+            {
+                // Let users choose their ink file using a file picker.
+                // Initialize the picker.
+                FileSavePicker savePicker = new FileSavePicker();
+                savePicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                savePicker.FileTypeChoices.Add("Portable Network Graphics Image", new List<string>() { ".png" });
+                savePicker.DefaultFileExtension = ".png";
+                savePicker.SuggestedFileName = "Untitled drawing";
+                StorageFile file = await savePicker.PickSaveFileAsync();
+                if (file != null)
+                {
+                    CachedFileManager.DeferUpdates(file);
+                    IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.ReadWrite);
+                    using (IOutputStream outputStream = stream.GetOutputStreamAt(0))
+                    {
+                        await DrawingCanvas.InkPresenter.StrokeContainer.SaveAsync(outputStream);
+                        await outputStream.FlushAsync();
+                    }
+                    stream.Dispose();
+                    FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+                    if (status == FileUpdateStatus.Complete)
+                    {
+                        /* ContentDialog FileSaved = new ContentDialog()
+                        {
+                            Title = "File saved as " + file.Name,
+                            CloseButtonText = "Close",
+                        }; */
+                        new ToastContentBuilder()
+                            .AddText("File saved successfully!")
+                            .AddText("Saved as " + file.Name);
+                    }
+                    else
+                    {
+                        /* ContentDialog FileNotSaved = new ContentDialog()
+                        {
+                            Title = "File wasn't saved.",
+                            Content="Are you sure you have access to the current path?",
+                            CloseButtonText = "Close",
+                        }; */
+                        new ToastContentBuilder()
+                            .AddText("Failed to save file")
+                            .AddText("Are you sure you have access to the current path?");
+                    }
+                }
+                // User selects Cancel and picker returns null.
+                else
+                {
+                    // Operation cancelled.
+                    /* ContentDialog FileSaveCancelled = new ContentDialog()
+                    {
+                        Title = "Operation cancelled by user.",
+                        CloseButtonText = "Close",
+                    }; */
+                    new ToastContentBuilder()
+                        .AddText("Operation cancelled by user");
+                }
+            }
+        }
+        private void ExportCancelled(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            new ToastContentBuilder().AddText("Saving cancelled");
+        }
+        private void DefaultSizeChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int DSPIndex = DefaultSizePicker.SelectedIndex;
+            switch(DSPIndex)
+            {
+                case 0: SavingHeight = 7680; SavingWidth = 4320; break;
+                case 1: SavingHeight = 3840; SavingWidth = 2160; break;
+                case 2: SavingHeight = 1920; SavingWidth = 1080; break;
+                case 3: SavingHeight = 1280; SavingWidth = 720; break;
+                case 4: SavingHeight = 960; SavingWidth = 540; break;
+                default: SavingHeight = 0; SavingWidth = 0; break;
+            }
+        }
+        private void CustomHeight_OnBeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            args.Cancel = args.NewText.Any(c => !char.IsDigit(c));
+        }
+        private void CustomWidth_OnBeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            args.Cancel = args.NewText.Any(c => !char.IsDigit(c));
+        }
+        private void CustomHeight_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            sender.Text = new String(sender.Text.Where(char.IsDigit).ToArray());
+        }
+        private void CustomWidth_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            sender.Text = new String(sender.Text.Where(char.IsDigit).ToArray());
+        }
+        private void CustomSizeSelected_Click(object sender, RoutedEventArgs e)
+        {
+            SavingHeight = Int32.Parse(CustomHeight.Text.ToString());
+            SavingWidth = Int32.Parse(CustomWidth.Text.ToString());
         }
         private void SettingsClick(object sender, RoutedEventArgs e)
         {
